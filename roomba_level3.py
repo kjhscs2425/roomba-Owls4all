d3441351235b554b4df96cac6e8c2a4ef0764db1
owls4all <owls4all@gmail.com>
2024-09-05 11:25:15 -0700

    Solved level 3

--- roomba_level3.py
+++ roomba_level3.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Athena
 # -----------------------------------------------------------------------------
  
 from turtle import right, left, forward, backward, speed
@@ -12,12 +12,36 @@ import room
 speed(7)
 
 # Draw the Level 3 version of the room
-window = room.draw_room(level = 3)
+window = room.draw_room(level = 3,radius=5)
 
 ###
 # Start your code here
- 
- 
+step=40
+r=5
+forward(step)
+right(90)
+for i in range(4):
+    forward((r-2)*step)
+    left(90)
+    forward(step)
+    right(90)
+    forward(step)
+    left(90)
+    forward((r-2)*step)
+    right(90)
+    forward(step)
+    backward(step)
+    left(90)    
+for i in range(3):
+    left(90)
+    forward(step)
+    right(90)
+    for j in range(4):
+        forward(step*(3-i))
+        left(90)
+        forward((3-i)*step)
+left(90)
+forward(step)
  
 # End your code here
 ###
